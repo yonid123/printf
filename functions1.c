62de8e6cca8f07dc51116e887f77d5b9da8a80e2
mvincentbb <mvincentbb@gmail.com>
2022-10-18 20:41:42 +0100

    fix print_octal

--- functions1.c
+++ functions1.c
@@ -38,7 +38,7 @@ int print_unsigned(va_list types, char buffer[],
 /************* PRINT UNSIGNED NUMBER IN OCTAL  ****************/
 /**
  * print_octal - Prints an unsigned number in octal notation
- * @types: Lista of arguments
+ * @types: List of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: get width
